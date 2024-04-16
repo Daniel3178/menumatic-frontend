8d3b44bc3d65fc20894f5ddd4fd659efff4ddf83
Gustav Landberg <landbergg@outlook.com>
2024-04-16 13:45:23 +0200

    Initial version of PDF generator at /pdf-test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import DanielTestPresenter from "./test/danielTestPresenter";
+import CreatePDFForm from "./pdfgen"
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div><h1>Hello World</h1></div>}/>
-        <Route path="/daniel-test" element={<DanielTestPresenter/>}/>
+        <Route path="/" element={<div><h1>Hello World</h1></div>} />
+        <Route path="/daniel-test" element={<DanielTestPresenter />} />
+        <Route path="/pdf-test" element={<CreatePDFForm />} />
       </Routes>
     </BrowserRouter>
   );
