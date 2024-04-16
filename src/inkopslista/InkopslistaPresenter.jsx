e0b034ef6010f359eadf6a792154f0c9bfe0186f
Dana Ghafour Fatulla <danagf@kth.se>
2024-04-16 16:35:08 +0200

    Jonathans kod

--- src/inkopslista/InkopslistaPresenter.jsx
+++ src/inkopslista/InkopslistaPresenter.jsx
@@ -15,17 +15,31 @@ function outObj(name, amount, unit){
     this.unit = unit
 }
 
+<<<<<<< Updated upstream
 
+=======
+const outObj = {
+    name:
+}
+>>>>>>> Stashed changes
 
 while(inArray[i] != null){
     while(inArray[i].extendedIngredients[j] != null){
             while(outArray[k] != null){
+<<<<<<< Updated upstream
                 addAmount = (inArray[i].extendedIngredients[j].amount * (wantedPortions / inArray[i].extendedIngredients[j].servings));
+=======
+                addAmount = (inArray[i].extendedIngredients[j].amount * (wantedPortions / inArray[i].servings));
+>>>>>>> Stashed changes
                 if(outArray[k].name == inArray[i].extendedIngredients[j].name){
                 outArray[k].amount += addAmount
                 }
                 else{
+<<<<<<< Updated upstream
                     outArray.last = {inArray[i].extendedIngredients[j].name, inArray[i].extendedIngredients[j].amount, inArray[i].extendedIngredients[j].unitShort}
+=======
+                    outArray.last = {inArray[i].extendedIngredients[j].name, addAmount, inArray[i].extendedIngredients[j].unitShort}
+>>>>>>> Stashed changes
                 }
                 k++
             }
