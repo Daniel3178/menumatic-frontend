f71e248eec15d081a5eb6dc795f223b6a4886bc7
Gustav Eriksson <gustavke@kth.se>
2024-04-16 16:07:40 +0200

    centered clock icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,5 @@
 import clock from "./clock.svg"
+import thumbs_up from "./thumbs_up.svg"
+import thumbs_down from "./thumbs_down.svg"
 
-export {clock}
+export {clock, thumbs_up, thumbs_down}
--- src/homepage/homePageView.jsx
+++ src/homepage/homePageView.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { clock } from "../assets";
+import { clock, thumbs_up, thumbs_down } from "../assets";
 
 const HomePageView = (props) => {
   console.log(props.apiResults);
@@ -7,14 +7,16 @@ const HomePageView = (props) => {
   const conditialRender = () => {
     if (props.apiResults) {
       return (
-        <div className="bg-gray-100 p-8 rounded-lg shadow-md">
-          <img src={props.apiResults.recipes[0].image} alt="food" className="rounded-lg" />
-          <h1 className="text-3xl font-bold mb-4">{props.apiResults.recipes[0].title}</h1>
+        <div className="bg-gray-100 p-8 rounded-lg shadow-md items-center justify-center">
+          <div className="flex flex-col items-center justify-center">
+            <img src={props.apiResults.recipes[0].image} alt="food" className="rounded-lg"  /> {/* Adjust width and height as needed */}
+            <h1 className="text-3xl font-bold mb-4 text-balance">{props.apiResults.recipes[0].title}</h1>
+          </div>
           <div className="flex items-center">
-            <img src={clock} alt="clock" className="w-6 h-6 mr-2 pb-2" /> {/* Adjust size as needed */}
+            <img src={clock} alt="clock" className="w-6 h-6 mr-2 mb-4" /> {/* Adjust size as needed */}
             <h1 className="text-3xl font-bold mb-4">{props.apiResults.recipes[0].readyInMinutes} min</h1>
           </div>
-        </div>
+        </div>  
       );
     }
   };
