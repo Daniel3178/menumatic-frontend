7b1db6b4d3d26db1fc81c0725edfd83c5cd6327b
Dana Ghafour Fatulla <danagf@kth.se>
2024-04-16 16:39:54 +0200

    Jonathans kod

--- src/inkopslista/InkopslistaPresenter.jsx
+++ src/inkopslista/InkopslistaPresenter.jsx
@@ -10,36 +10,26 @@ const outArray = [];
 const inArray = [object:wantedPortions];
 
 function outObj(name, amount, unit){
-    this.name = name
-    this.amount = amount
-    this.unit = unit
+    return {
+        name: name,
+        amount: amount,
+        unit: unit
+    };
 }
 
-<<<<<<< Updated upstream
-
-=======
 const outObj = {
     name:
 }
->>>>>>> Stashed changes
 
 while(inArray[i] != null){
     while(inArray[i].extendedIngredients[j] != null){
             while(outArray[k] != null){
-<<<<<<< Updated upstream
-                addAmount = (inArray[i].extendedIngredients[j].amount * (wantedPortions / inArray[i].extendedIngredients[j].servings));
-=======
                 addAmount = (inArray[i].extendedIngredients[j].amount * (wantedPortions / inArray[i].servings));
->>>>>>> Stashed changes
                 if(outArray[k].name == inArray[i].extendedIngredients[j].name){
                 outArray[k].amount += addAmount
                 }
                 else{
-<<<<<<< Updated upstream
-                    outArray.last = {inArray[i].extendedIngredients[j].name, inArray[i].extendedIngredients[j].amount, inArray[i].extendedIngredients[j].unitShort}
-=======
                     outArray.last = {inArray[i].extendedIngredients[j].name, addAmount, inArray[i].extendedIngredients[j].unitShort}
->>>>>>> Stashed changes
                 }
                 k++
             }
