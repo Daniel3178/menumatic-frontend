65d5793ce7096ea5521315ba5f29521bb08516ba
Victor Naumburg <41002203+adsapientiam@users.noreply.github.com>
2024-04-17 08:56:12 +0200

    Update RecepiesForAWeekView.jsx

--- src/RecepiesForAWeekView.jsx
+++ src/RecepiesForAWeekView.jsx
@@ -4,12 +4,12 @@ import React from 'react'
 const RecepieForADay = (props) => {
 
     return (
-        <ul class="recepie-for-a-day">
-            <li>Day {props.dayOfTheWeek}</li>
-            <li>{props.dish}</li>
-            <li>{props.priceLevel}</li>
-            <li><input type="number" id={props.dayOfTheWeek} name="quantity" min="1" value="1"></input></li>
-        </ul>
+        <tr class="recepie-for-a-day">
+            <td>Day {props.dayOfTheWeek}</td>
+            <td>{props.dish}</td>
+            <td>{props.priceLevel}</td>
+            <td><input type="number" id={props.dayOfTheWeek} name="quantity" min="1" value="1"></input></td>
+        </tr>
     )
 }
 
@@ -17,15 +17,29 @@ const RecepieForADay = (props) => {
 // CSS on the list into more fancy
 const RecepiesForWeek = (props) => {
   return (
-    <ul>
-        <li><RecepieForADay dayOfTheWeek="1" dish={props.RecepiesForWeekData[0].dish} priceLevel={props.RecepiesForWeekData[0].priceLevel}></RecepieForADay></li>
-        <li><RecepieForADay dayOfTheWeek="2" dish={props.RecepiesForWeekData[1].dish} priceLevel={props.RecepiesForWeekData[1].priceLevel}></RecepieForADay></li>
-        <li><RecepieForADay dayOfTheWeek="3" dish={props.RecepiesForWeekData[2].dish} priceLevel={props.RecepiesForWeekData[2].priceLevel}></RecepieForADay></li>
-        <li><RecepieForADay dayOfTheWeek="4" dish={props.RecepiesForWeekData[3].dish} priceLevel={props.RecepiesForWeekData[3].priceLevel}></RecepieForADay></li>
-        <li><RecepieForADay dayOfTheWeek="5" dish={props.RecepiesForWeekData[4].dish} priceLevel={props.RecepiesForWeekData[4].priceLevel}></RecepieForADay></li>
-        <li><RecepieForADay dayOfTheWeek="6" dish={props.RecepiesForWeekData[5].dish} priceLevel={props.RecepiesForWeekData[5].priceLevel}></RecepieForADay></li>
-        <li><RecepieForADay dayOfTheWeek="7" dish={props.RecepiesForWeekData[6].dish} priceLevel={props.RecepiesForWeekData[6].priceLevel}></RecepieForADay></li>
-    </ul>
+    <div>
+      <ul>
+        <li>Kateogri 1 att göra till horisonetll lista</li>
+        <li>Kategori 2</li>
+        <li>Kategori 3</li>
+      </ul>
+      <table>
+          <RecepieForADay dayOfTheWeek="1" dish={props.RecepiesForWeekData[0].dish} priceLevel={props.RecepiesForWeekData[0].priceLevel}></RecepieForADay>
+          <RecepieForADay dayOfTheWeek="2" dish={props.RecepiesForWeekData[1].dish} priceLevel={props.RecepiesForWeekData[1].priceLevel}></RecepieForADay>
+          <RecepieForADay dayOfTheWeek="3" dish={props.RecepiesForWeekData[2].dish} priceLevel={props.RecepiesForWeekData[2].priceLevel}></RecepieForADay>
+          <RecepieForADay dayOfTheWeek="4" dish={props.RecepiesForWeekData[3].dish} priceLevel={props.RecepiesForWeekData[3].priceLevel}></RecepieForADay>
+          <RecepieForADay dayOfTheWeek="5" dish={props.RecepiesForWeekData[4].dish} priceLevel={props.RecepiesForWeekData[4].priceLevel}></RecepieForADay>
+          <RecepieForADay dayOfTheWeek="6" dish={props.RecepiesForWeekData[5].dish} priceLevel={props.RecepiesForWeekData[5].priceLevel}></RecepieForADay>
+          <RecepieForADay dayOfTheWeek="7" dish={props.RecepiesForWeekData[6].dish} priceLevel={props.RecepiesForWeekData[6].priceLevel}></RecepieForADay>
+          <tr>
+            <td>Default portions</td>
+            <td><input type="number" id="defaultPortions" name="quantity" min="1" value="1"></input></td>
+          </tr>
+      </table>
+
+      <input type="submit" value="✔ Shopping list."></input>
+
+    </div>
   )
 }
 
