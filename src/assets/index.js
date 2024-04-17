f998e7a615c7ef19220d77fc56fcd5a20c427dba
Gustav Eriksson <gustavke@kth.se>
2024-04-17 14:31:58 +0200

    Added button and functionality for additional recipe info

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,8 @@ import clock from "./clock.svg"
 import thumbs_up from "./thumbs_up.svg"
 import thumbs_down from "./thumbs_down.svg"
 import info from "./info.svg"
+import info_i from "./info_i.svg"
+import close from "./close.svg"
+import tune from "./tune.svg"
 
-export {clock, thumbs_up, thumbs_down, info}
+export {clock, thumbs_up, thumbs_down, info, info_i, close, tune}
--- src/homepage/homePageView.jsx
+++ src/homepage/homePageView.jsx
@@ -1,62 +1,75 @@
 import React from 'react';
-import { clock, thumbs_up, thumbs_down, info } from "../assets";
+import { clock, thumbs_up, thumbs_down, info, close, info_i, tune } from "../assets";
 
 const HomePageView = (props) => {
   console.log(props.apiResults);
-  
+
   // TODO: Change apiResults[0] to apiResults.recipes[0] accross the file to work with the API
-  const conditialRender = () => {
+
+
+  const conditionalRender = () => {
+    if (props.info) {
+      return (
+
+        <div className="flex flex-col items-center justify-center h-screen">
+          <div className="w-[550px] h-[550px] bg-gray-100 rounded-xl shadow-md overflow-auto">
+            <div className="p-6">
+              <h1 className="text-3xl font-bold mb-4 text-balance">{props.apiResults[0].title} </h1>
+              <div>
+                <div className="flex items-center">
+                  <img src={clock} alt="clock" className="w-6 h-6 mr-2 mb-4" /> {/* Adjust size as needed */}
+                  <h1 className="text-3xl font-bold mb-4">{props.apiResults[0].readyInMinutes} min</h1>
+                </div>
+                <h2 className="text-xl font-semibold mb-2">Ingredients:</h2>
+                <ul className="h-50 list-disc pl-5 overflow-auto">
+                  {props.apiResults[0].extendedIngredients.map((item, index) => (
+                    <li key={index} className="text-lg">{item.name}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+          <button onClick={props.toggleInfoView} className=" hover:scale-110 shadow-md m-2 bg-gray-100 hover:bg-gray-200 rounded-full w-20 h-20 flex items-center justify-center focus:outline-none">
+            <img src={close} alt="Image" className="w-10 h-10 rounded-full" />
+          </button>
+        </div>
+      );
+    }
     if (props.apiResults) {
       return (
-        <div className="bg-gray-100 p-8 rounded-lg shadow-md">
-          <img src={props.apiResults[0].image} alt="food" className="rounded-lg" />
-          <h1 className="text-3xl font-bold mb-4">{props.apiResults[0].title}</h1>
-          <div className="flex items-center">
-            <img src={clock} alt="clock" className="w-6 h-6 mr-2 pb-2" /> {/* Adjust size as needed */}
-            <h1 className="text-3xl font-bold mb-4">{props.apiResults[0].readyInMinutes} min</h1>
+        <div className="flex flex-col items-center justify-center h-screen">
+          <div className="w-[550px] h-[550px] bg-gray-100 p-8 rounded-lg shadow-md">
+            <img src={props.apiResults[0].image} alt="food" className="rounded-lg" />
+            <h1 className="text-3xl font-bold mb-4">{props.apiResults[0].title}</h1>
+            <div className="flex items-center">
+              <img src={clock} alt="clock" className="w-6 h-6 mr-2 pb-2" /> {/* Adjust size as needed */}
+              <h1 className="text-3xl font-bold mb-4">{props.apiResults[0].readyInMinutes} min</h1>
+            </div>
           </div>
-        </div>  
+          <button onClick={props.toggleInfoView} className="hover:scale-110 shadow-md m-2 bg-gray-100 hover:bg-gray-200 rounded-full w-20 h-20 flex items-center justify-center focus:outline-none">
+            <img src={info_i} alt="Image" className="w-7 h-7 rounded-full" />
+          </button>
+        </div>
+
       );
     }
   };
 
-  if(true){
-    
-    return(<div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-4 text-balance">{props.apiResults.recipes[0].title}</h1>
-      <div>
-      <div className="flex items-center">
-            <img src={clock} alt="clock" className="w-6 h-6 mr-2 mb-4" /> {/* Adjust size as needed */}
-            <h1 className="text-3xl font-bold mb-4">{props.apiResults.recipes[0].readyInMinutes} min</h1>
-          </div>
-        <h2 className="text-xl font-semibold mb-2">Ingredients:</h2>
-        <ul className="list-disc pl-5">
-          {props.apiResults.recipes[0].extendedIngredients.map((item, index) => (
-            <li key={index} className="text-lg">{item.name}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  </div>
-);
-  }
-
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      {conditialRender()}
-      <div className="mb-8 flex flex-col items-center">
-
-        <button  className="m-2 bg-gray-100 hover:bg-gray-200 rounded-full w-20 h-20 flex items-center justify-center focus:outline-none">
-          <img src={info} alt="Image" className="w-10 h-10 rounded-full"/>
+      <div className=" w-[550px] flex justify-end">
+        <button className="flex items-center justify-end m-2 bg-white  w-20 h-20 focus:outline-none">
+          <img src={tune} alt="Image" className="w-10 h-10 rounded-full hover:scale-110" />
         </button>
-
+      </div>
+      {conditionalRender()}
+      <div className="mb-8 flex flex-col items-center ">
         <div className="flex items-center">
-          <button onClick={props.getRandomReceipt} className="mt-20 mr-20 bg-red-500 hover:bg-red-600 rounded-full w-40 h-40 flex items-center justify-center focus:outline-none">
-            <img src={thumbs_down} alt="Image" className="w-20 h-20 rounded-full"/>
+          <button onClick={props.getRandomReceipt} className="hover:scale-105 shadow-md mt-15 mr-20 bg-red-500 hover:bg-red-600 rounded-full w-40 h-40 flex items-center justify-center focus:outline-none">
+            <img src={thumbs_down} alt="Image" className="w-20 h-20 rounded-full" />
           </button>
-          <button onClick={props.sendLike} className="mt-20 ml-20 bg-green-500 hover:bg-green-600 rounded-full w-40 h-40 flex items-center justify-center focus:outline-none">
-            <img src={thumbs_up} alt="Image" className="w-20 h-20 rounded-full"/>
+          <button onClick={props.sendLike} className="hover:scale-105 shadow-md mt-15 ml-20 bg-green-500 hover:bg-green-600 rounded-full w-40 h-40 flex items-center justify-center focus:outline-none">
+            <img src={thumbs_up} alt="Image" className="w-20 h-20 rounded-full" />
           </button>
         </div>
       </div>
