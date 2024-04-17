7d74688159858650ff72b216baf6c7adfb9e90c7
jonathansson <jonathansson98@gmail.com>
2024-04-17 15:58:55 +0200

    Ingredients, amount and units passed
    JAAAAAAAAAAA

--- src/shoplist/shoplistSlice.js
+++ src/shoplist/shoplistSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { connectStorageEmulator } from "firebase/storage";
 
 const shoplistSlice = createSlice({
   name: "Shoplist",
@@ -7,49 +8,46 @@ const shoplistSlice = createSlice({
   },
   reducers: {
     generateShoplist: (state, action) => {
-        console.log("InkopslistaPresenter, shoplistObject, InArray, ERROR here ");
+        //console.log("InkopslistaPresenter, shoplistObject, InArray, ERROR here ");
         
       let inArray = action.payload;
       let outArray = [];
-      outArray.push({name: "ingredient", amount: "amount", unit: "unit"})
-      console.log(inArray[0]);
-      for (let i = 0; i < inArray.length; i++) {
-        console.log("list of recipes for each day")
-        console.log(inArray[i])
-        for (let j = 0; j < inArray[i].result.extendedIngredients.length; j++) {
-            console.log("ingredients of recipe")
-            console.log("ingredient array size: " + inArray[i].result.extendedIngredients.length)
-            console.log("outArray length: " + outArray.length)
-          for (let k = 0; k <= outArray.length; k++) {
-            console.log("[STATE 1] check for already existing ingredients")
-            let addAmount =
-              inArray[i].result.extendedIngredients[j].amount *
-              (inArray[i].portions / inArray[i].result.servings);
-              console.log("addAmount: " + addAmount)
-            if (outArray[k].name === inArray[i].result.extendedIngredients[j].name) {
-                console.log("if statement")
-              outArray[k].amount += addAmount;
-              console.log(outArray);
-            } else {
-                console.log("else statement")
-              outArray.push(
-                {
-                  name: inArray[i].result.extendedIngredients[j].name,
-                  amount: addAmount,
-                  unit: inArray[i].result.extendedIngredients[j].unitShort
-                }
-              );
-              console.log(outArray);
+
+      const stripIngr = (meal) => {
+          const portions = meal.portions
+          const servings = meal.result.servings
+          const ingredients = meal.result.extendedIngredients
+          const ingrArr = []
+          for(let i = 0; i < ingredients.length; i++){
+            const calcAmount = ingredients[i].amount * (portions / servings)
+            ingrArr.push({name: ingredients[i].originalName, amount: calcAmount, unit: ingredients[i].unitShort})
+        }
+        return ingrArr
+      }
+      console.log(stripIngr(action.payload[0]))
+
+      const updateAllItems = (props) => {
+        for(let i = 0; i < props.length; i++){
+          let flag = -1
+          for(let j = 0; j < state.allItems.length; j++){
+            if(props[i].name == state.allItems[j]?.name){
+              allItems[j].amount += props[i].amount
+              flag = 1
             }
+          }
+          if(flag = -1){
+            state.allItems.push(props[i])
           }
         }
       }
-      console.log("EROR maybe here ")
-      console.log(outArray);
-      state.allItems = outArray;
-    },
-  },
-});
+      
+      for(let i = 0; i < action.payload.length; i++){
+        updateAllItems(stripIngr(action.payload[i]))
+      }
+     },
+   },
+ }
+);
 export const getAllItems = (state) => state.shoplist.allItems;
 export const { generateShoplist } = shoplistSlice.actions;
 export default shoplistSlice.reducer;
