0e9dedf735dbb8a6a2e2ac243a049b5c21981e42
NoahTornqvist <noah.toernqvist@gmail.com>
2024-04-18 14:25:11 +0200

    added funtion to save filters

--- src/filterpage/filterPagePresenter.jsx
+++ src/filterpage/filterPagePresenter.jsx
@@ -4,18 +4,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getApiResults, searchBySpoonacularApiAsync } from '../store/spoonacularAPISlice';
 import {} from './filterPageSlice';
 import { addToReocemmendationList } from '../recommendation_page/recommendationPageSlice';
+import { saveBoundel1, saveBoundel2 } from "./filterPageSlice";
 import { useNavigate } from 'react-router-dom';
 import { objects } from '../assets/constObjects';
 
 const FilterPagePresenter = () => {
 
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const handleApplyFilter = (includeTags, excludeTags) => {
-    // TODO: send data to api slice
-    console.log("Filter button pressed")
-    console.log("include Tags: " + includeTags.toString());
-    console.log("exclude Tags: " + excludeTags.toString());
+    dispatch(saveBoundel1(includeTags))
+    dispatch(saveBoundel2(excludeTags))
     navigate("/homepage-test")
   }
 
--- src/filterpage/filterPageSlice.js
+++ src/filterpage/filterPageSlice.js
@@ -1,13 +1,29 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-const filterPage = createSlice({
+export const filterPage = createSlice({
     name: "filterPage",
     initialState: {
-        likesCounter: 0,
-        showInfo: false,
+        apiPrefs: {
+            boundel1: {
+                title: "food preference",
+                paramsArray: [],
+            },
+            boundel2: {
+                title: "allergies",
+                paramsArray: [],
+            }
+        }
     },
     reducers: {
-        
-
+        saveBoundel1: (state, action) => {
+            state.apiPrefs.boundel1.paramsArray.push(action.payload)
+        },
+        saveBoundel2: (state, action) => {
+            state.apiPrefs.boundel2.paramsArray.push(action.payload)
+        },
     },
     });
+
+    export default filterPage.reducer;
+
+    export const {saveBoundel1, saveBoundel2} = filterPage.actions;
