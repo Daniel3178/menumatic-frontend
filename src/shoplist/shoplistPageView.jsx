f9a9e15afeddc982ce9323da61956a53475fb810
jonathansson <jonathansson98@gmail.com>
2024-04-18 09:42:33 +0200

    css to be tested

--- src/shoplist/shoplistPageView.jsx
+++ src/shoplist/shoplistPageView.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-const ShoplistPageView = (props) => {
-  const renderIngr = ()=>{
-    return props.allItems.map((ingre)=>{
-      return(<div>
-        {ingre.name}
-      </div>)
-    })
-  }
+const ShoplistPageView = ({ allItems }) => {
   return (
-    <div>
-      {renderIngr()}
+    <div className="container mx-auto px-4">
+      <h2 className="text-2xl font-bold mb-4">Shopping List</h2>
+      <ul>
+        {allItems.map((item, index) => (
+          <li key={index} className="mb-2">
+            <span className="mr-2">{item.amount} {item.unit}</span> - {item.name}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default ShoplistPageView
+export default ShoplistPageView;
