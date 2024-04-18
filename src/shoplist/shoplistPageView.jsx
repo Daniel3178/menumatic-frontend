62ce7513ca0a2abc3a242ea19888863bd75439e2
Dana Ghafour Fatulla <danagf@kth.se>
2024-04-18 10:58:22 +0200

    shopListView

--- src/shoplist/shoplistPageView.jsx
+++ src/shoplist/shoplistPageView.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 
 const ShoplistPageView = ({ allItems }) => {
   return (
-    <div className="container mx-auto px-4">
-      <h2 className="text-2xl font-bold mb-4">Shopping List</h2>
-      <ul>
-        {allItems.map((item, index) => (
-          <li key={index} className="mb-2">
-            <span className="mr-2">{item.amount} {item.unit}</span> - {item.name}
-          </li>
-        ))}
-      </ul>
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold text-center mb-4">Inköpslista</h1>
+      <div className="bg-white shadow-md rounded-lg py-4 px-6">
+        {allItems.length > 0 ? (
+          <ul className="space-y-2">
+            {allItems.map((ingredient, index) => (
+              <li key={index} className="flex justify-between items-center border-b pb-2">
+                <span className="text-sm text-gray-500">{`${ingredient.amount} ${ingredient.unit}`}</span>
+                <span className="text-lg text-gray-700">{ingredient.name}</span>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="text-gray-700 text-center">Ingen inköpslista tillgänglig.</p>
+        )}
+      </div>
     </div>
   );
 };
