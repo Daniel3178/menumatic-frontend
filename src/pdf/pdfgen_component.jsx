a9ce6271663e57b1a07eba8a4e9a9f7a838a819b
Gustav Landberg <landbergg@outlook.com>
2024-04-18 10:49:25 +0200

    slight refactor

---
+++ src/pdf/pdfgen_component.jsx
@@ -0,0 +1,180 @@
+
+import React from 'react'
+
+import { PDFViewer, Page, Text, Rect, View, Document, StyleSheet, PDFDownloadLink, Font } from '@react-pdf/renderer';
+
+
+
+const inArrayExample = [['pärlsocker', 100, 'g'], ['socker', 100, 'g'], ['smör', 100, 'g'], ['kaffe', 10, 'g'], ['cocoa', 100, 'g'], ['oatmeal,', 100, 'g']];
+
+
+Font.register({
+  family: "Roboto",
+  src:
+    "https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-light-webfont.ttf"
+});
+
+const styles = StyleSheet.create({
+  headline: {
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 20
+  },
+  headline2: {
+    fontSize: 20
+  },
+  mainbody: {
+    fontSize: 14,
+    color: "#aaaaaa",
+  },
+
+  page: {
+    fontFamily: "Roboto",
+    //flexDirection: 'row',
+    backgroundColor: '#FFFFFF'
+  },
+  listItem: {
+    flexDirection: "row",
+    marginBottom: 4,
+    textAlign: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    fontSize: 13,
+  },
+  bullet: {
+    fontSize: 13,
+    marginRight: 5,
+    textAlign: 'center',
+    alignSelf: 'center',
+  },
+  section: {
+    margin: 30,
+    padding: 10,
+    flexGrow: 1
+  }
+});
+
+const styles_recipe = StyleSheet.create({
+  headline: {
+    fontSize: 24,
+    textAlign: "right",
+    marginTop: 20,
+    marginRight: 30,
+    marginBottom: 20
+  },
+  headline_section: {
+
+  },
+  headline2: {
+    fontSize: 20
+  },
+  mainbody: {
+    fontSize: 14,
+    color: "#aaaaaa",
+  },
+
+  page: {
+    fontFamily: "Roboto",
+    flexDirection: 'row',
+    backgroundColor: '#FFFFFF'
+  },
+  listItem: {
+    flexDirection: "row",
+    marginBottom: 4,
+    alignItems: "center",
+    alignSelf: "center",
+    fontSize: 10,
+  },
+  bullet: {
+    fontSize: 10,
+    marginRight: 5,
+    textAlign: 'right',
+    alignSelf: 'right',
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+});
+const ListItem = ({ children, dot = '•', stylesheet = styles }) => {
+  return (
+    <View style={{ stylesheet }.listItem}>
+      <Text style={{ stylesheet }.bullet}>{dot + children}</Text>
+    </View>
+  );
+};
+// Create Document Componentit
+const Recipes = ({ title, ingredients, instructions }) => (
+  <Document>
+    <Page size="A5" orientation={"landscape"} style={styles_recipe.page}>
+      <Text style={styles_recipe.mainbody}> by menumatic</Text>
+      <View style={styles_recipe.headline_section} >
+        <Text style={styles_recipe.headline}>{title}</Text>
+      </View>
+      <View style={styles_recipe.section}>
+        <Text style={styles_recipe.headline2}>Ingredients:</Text>
+        <BulletList items={ingredients} stylesheet={styles_recipe} />
+      </View>
+      <View style={styles_recipe.section}>
+        <Text style={styles_recipe.headline2}>Instructions:</Text>
+        <BulletList items={instructions} dot={""} stylesheet={styles_recipe} />
+      </View>
+    </Page>
+  </Document >
+);
+
+
+const BulletList = ({ items, dot = "•", stylesheet = styles }) => {
+  return (
+    <View>
+      {items.map((item) => (
+        <ListItem key={item.id || Math.random()} dot={dot} stylesheet={stylesheet}>{item}</ListItem>
+      ))}
+    </View>
+  );
+}
+
+
+
+const ShoppingList = ({ ingredients }) => (
+  <Document>
+    <Page size="A6" style={styles.page}>
+      <View>
+        <Text style={styles.mainbody}> by menumatic</Text>
+        <Text style={styles.headline}>Shopping List</Text>
+        <BulletList items={ingredients} />
+      </View>
+    </Page>
+  </Document>
+);
+function CreatePDFForm() {
+  const [inputText, setInputText] = React.useState("");
+
+
+  function handleChange(event) {
+    setInputText(event.target.value);
+  }
+
+  var ingredientsList = inArrayExample.map((x) => x.join(' '));
+  console.log(ingredientsList);
+  return (
+    <div>
+      <PDFDownloadLink document={<Recipes title="chokladbollar" ingredients={ingredientsList} instructions={["1. gör chokladbollarna"]} />} fileName="recipes.pdf">
+        {({ blob, url, loading, error }) =>
+          loading ? 'Loading document...' : 'Download Recipes'
+        }
+      </PDFDownloadLink>
+      <br></br><br></br>
+      <PDFDownloadLink document={<ShoppingList ingredients={ingredientsList} />} fileName="ShoppingList.pdf">
+        {({ blob, url, loading, error }) =>
+          loading ? 'Loading document...' : 'Download Shoppling List'
+        }
+      </PDFDownloadLink>
+    </div>
+  );
+}
+
+
+export default CreatePDFForm;
+
