23ea39a30ed4b5f32a8f6a192f5af8b074f2c3ee
Dana Ghafour Fatulla <danagf@kth.se>
2024-04-23 15:53:13 +0200

    Work on planSlice

--- src/listOfStoredPlansRelated/plan_content/planSlice.js
+++ src/listOfStoredPlansRelated/plan_content/planSlice.js
@@ -1,59 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { incrementLikesCounter } from "../homepage/homePageSlice";
 
-const recommendation = createSlice({
-  name: "recommendation",
+const initialState = {
+  selectedListId: null,
+};
+
+const planSlice = createSlice({
+  name: "plan",
   initialState: {
-    recommendationList: [],
+    selectedList: null,
+    allRecipes: []
   },
   reducers: {
-    /**
-     * [deprecated]
-     * adds dish to recommendationList
-     * @param {*} state: state of the slice
-     * @param {*} action: a dish object to be added to recommendationList
-     * by Daniel
-     */
-    addToReocemmendationList: (state, action) => {
-      state.recommendationList.push({ count: 1, result: action.payload });
+    setSelectedListId: (state, action) => {
+      state.selectedListId = action.payload;
     },
-    /*
-     * Parameters: state: the current state. action: The dispatched action to be performed.
-     * Event: Iterates over each element in the recommendationList array.
-     * Condition (if-check): If the id of the currently iterated item in the recommendationList matches the payload's id.
-     * Then: Update the count of the currently iterated item (in recommendationList) with the instructed count.
-     */
-    updateCount: (state, action) => {
-      state.recommendationList.map((item) => {
-        if (item.result.id === action.payload.id) {
-          item.count = action.payload.count;
-        }
-      });
-    },
-  },
-  /*
-   * The first case listen to increamentLikesCounter from homePageSlice.js and adds its argument to
-   * recommendation list.
-   */
-  extraReducers: (builder) => {
-    builder.addCase(incrementLikesCounter, (state, action) => {
-      state.recommendationList.push({
-        portions: 1,
-        result: { ...action.payload[0] },
-      });
-    });
   },
 });
 
+// Action creators are generated for each case reducer function
+export const { setSelectedListId } = planSlice.actions;
+
 /**
- * Purpose: Takes the actions retrieved from the recommendation slice and sets them to fixed variables.
- * Export reducers in the slice
- */
-export const { addToReocemmendationList, updateCount } = recommendation.actions;
-/**
- * Purpose: Returns the recommendationList from the state
+ * Purpose: Returns the selected list ID from the state
  * @param {*} state: the store
  */
-export const getPlanRecommendationList = (state) =>
-  state.recommendation.recommendationList;
-export default recommendation.reducer;
+export const getSelectedListId = (state) => state.plan.selectedListId;
+
+export default planSlice.reducer;
