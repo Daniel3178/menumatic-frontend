dad79e8ae22bcc02df24b81e1362eb466c3b633a
NoahTornqvist <noah.toernqvist@gmail.com>
2024-04-25 16:47:09 +0200

    logo, card, dislike, like buttons

--- src/homepage/homePageView.jsx
+++ src/homepage/homePageView.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { clock, thumbs_up, thumbs_down, close, info_i, tune, noimage } from "../assets";
+import { clock, thumbs_up, thumbs_down, info, close, info_i, tune, dislike_btn, like_btn, logo, clock_icon, noimage } from "../assets";
 
 const HomePageView = (props) => {
-  
+
   // TODO: Change apiResults[0] to apiResults.recipes[0] accross the file to work with the API
   //conditionalRender either renders the info view for the current dish, or the photo for the current dish.
   const conditionalRender = () => {
 
-    
+
     //if info has been toggled the infopage is shown.
 
     if (props.info) {
@@ -40,71 +40,66 @@ const HomePageView = (props) => {
     if (props.apiResults) {
       //console.log(props.apiResults)
       return (
-        <div className="flex flex-col items-center justify-center h-screen">
-          <div className="w-[550px] h-[550px] bg-gray-100 p-8 rounded-lg shadow-md">
-            <img src={"https://img.spoonacular.com/recipes/" + props.apiResults[0].id + "-636x393." + props.apiResults[0].imageType} 
-            onError={({ currentTarget }) => {
-              currentTarget.onerror = null;
-              currentTarget.src=noimage;
-            }}
-            alt="food" 
-            className="rounded-lg" />
-            <h1 className="text-3xl font-bold mb-4">{props.apiResults[0].title}</h1>
-            <div className="flex items-center">
-              <img src={clock} alt="clock" className="w-6 h-6 mr-2 pb-2" /> 
-              <h1 className="text-3xl font-bold mb-4">{props.apiResults[0].readyInMinutes} min</h1>
+        <div className="flex justify-center">
+          <div className="w-405 h-540 rounded-large relative shadow-xl">
+            <img src={"https://img.spoonacular.com/recipes/" + props.apiResults[0].id + "-636x393." + props.apiResults[0].imageType} className="object-cover w-full h-full rounded-large" alt="Example Image" />
+            <div className="absolute inset-x-0 bottom-0 bg-cerulean bg-opacity-50 backdrop-blur-sm rounded-b-large w-405 h-130">
+              <div className='mt-2 ml-2'>
+                <p className="text-whiteSmoke font-outfit text-2xl font-medium">{props.apiResults[0].title}</p>
+              </div>
+              <div className='mb-4 absolute inset-x-0 bottom-0 flex space-x-4 items-center'>
+                <div className='mt-2 ml-2 flex text-whiteSmoke'>
+                  <img src={clock_icon} className="" />
+                </div>
+                <div className='text-whiteSmoke font-outfit text-2xl font-thin'>
+                  {props.apiResults[0].readyInMinutes} min
+                </div>
+              </div>
             </div>
           </div>
-          <button onClick={props.toggleInfoView} className="hover:scale-110 shadow-md m-2 bg-gray-100 hover:bg-gray-200 rounded-full w-20 h-20 flex items-center justify-center focus:outline-none">
-            <img src={info_i} alt="Image" className="w-7 h-7 rounded-full" />
-          </button>
         </div>
 
       );
     }
   };
 
-  const renderHomePage = () =>{
-    if(props.apiResultsState === "ready"){
-      return(
-        <div className="flex flex-col items-center justify-center h-screen">
-        <div className=" w-[550px] flex justify-end">
-          <button onClick={props.navigateToFilterPage} className="flex items-center justify-end m-2 bg-white  w-20 h-20 focus:outline-none">
-            <img src={tune} alt="Image" className="w-10 h-10 rounded-full hover:scale-110" />
-          </button>
-        </div>
-        {conditionalRender()}
-        <div className="mb-8 flex flex-col items-center ">
-          <div className="flex items-center">
-            <button onClick={props.getRandomReceipt} className="hover:scale-105 shadow-md mt-15 mr-20 bg-red-500 hover:bg-red-600 rounded-full w-40 h-40 flex items-center justify-center focus:outline-none">
-              <img src={thumbs_down} alt="Image" className="w-20 h-20 rounded-full" />
+  const renderHomePage = () => {
+    if (props.apiResultsState === "ready") {
+      return (
+        <div className="bg-page min-h-screen w-full fixed top-0 right-0 bottom-0 left-0">
+          <div className="flex justify-center w-444 h-102 mt-8 mb-16">
+            <img src={logo} />
+          </div>
+          {conditionalRender()}
+          <div className="flex space-x-20 justify-center mt-8">
+            <button onClick={props.sendLike} className="flex rounded-full shadow-xl hover:shadow-mid foucs:shadow-in">
+              <img src={like_btn} />
             </button>
-            <button onClick={props.sendLike} className="hover:scale-105 shadow-md mt-15 ml-20 bg-green-500 hover:bg-green-600 rounded-full w-40 h-40 flex items-center justify-center focus:outline-none">
-              <img src={thumbs_up} alt="Image" className="w-20 h-20 rounded-full" />
+            <button onClick={props.getRandomReceipt} className="flex rounded-full shadow-xl hover:shadow-mid foucs:shadow-in">
+              <img src={dislike_btn} />
             </button>
           </div>
         </div>
-      </div>
       )
     }
-    else if(props.apiResultsState === "loading"){
-      return(
+    else if (props.apiResultsState === "loading") {
+      return (
         <div className="flex flex-col items-center justify-center h-screen">
           <h1 className="text-3xl font-bold mb-4">Loading...</h1>
         </div>
       )
     }
-    else{
-      return(
+    else {
+      return (
         <div>Not ready</div>
       )
     }
   }
 
   return (
-<div>
-  {renderHomePage()}
-</div>
+    <div>
+      {renderHomePage()}
+    </div>
   );
 };
 
