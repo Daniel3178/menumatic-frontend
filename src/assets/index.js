0e08c39f0cc7e4fd9a676024cee5721cd2283dce
NoahTornqvist <noah.toernqvist@gmail.com>
2024-04-25 14:57:22 +0200

    homepage card with info, like and dislike buttons, logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import clock from "./clock.svg"
+import clock_icon from "./clock_icon.svg"
 import thumbs_up from "./thumbs_up.svg"
 import thumbs_down from "./thumbs_down.svg"
 import info from "./info.svg"
@@ -6,5 +7,8 @@ import info_i from "./info_i.svg"
 import close from "./close.svg"
 import tune from "./tune.svg"
 import done from "./done.svg"
+import like_btn from "./like_btn.svg"
+import dislike_btn from "./dislike_btn.svg"
+import logo from "./MENUMATIC_logo.svg"
 
-export {clock, thumbs_up, thumbs_down, info, info_i, close, tune, done}
+export {clock, thumbs_up, thumbs_down, info, info_i, close, tune, done, dislike_btn, like_btn, logo, clock_icon}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,41 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundColor: theme => ({
+        'page': '#F5F5F5',
+      }),
       width: {
-        '300': '40rem',
+        '405': '405px',
       },
-      Height: {
-        '300': '40rem',
-      }
-    },
+      height: {
+        '540': '540px',
+        '130': '130px'
+      },
+      colors: {
+        cerulean: '#4281A4',
+        yellowGreen: '#BDD358',
+        gunmetal: '#172E3B',
+        vanilla: '#FFEAAE',
+        whiteSmoke: '#F5F5F5',
+      },
+      boxShadow: {
+        'xl': '0px 20px 25px -5px rgba(0, 0, 0, 0.10), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)',
+        'mid': '0px 4px 6px -2px rgba(0, 0, 0, 0.05), 0px 10px 15px -3px rgba(0, 0, 0, 0.10)',
+        'in': '0px 4px 80px 0px rgba(0, 0, 0, 0.25) inset',
+      },
+      borderRadius: {
+        'large': '20px',
+      },
+      fontFamily: {
+        'outfit': ['outfit', 'sans-serif'],
+      },
+      fontSize: {
+        sm: ['14px', '20px'],
+        base: ['16px', '24px'],
+        lg: ['20px', '28px'],
+        xl: ['24px', '32px'],
+      },
+    }
   },
   plugins: [],
 }
-
