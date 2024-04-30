f994155284f74347ca7941a8f2e4ed2a221816d0
jonathansson <jonathansson98@gmail.com>
2024-04-30 16:25:49 +0200

    Signup menu page

--- src/assets/index.js
+++ src/assets/index.js
@@ -13,7 +13,8 @@ import logo from "./MENUMATIC_logo.svg"
 import noimage from "./noimage.png"
 import backGreen from "./backGreen.svg"
 import closeBtn from "./closeBtn.svg"
+import backBlue from "./backBlue.svg"
 
-export {clock, closeBtn, thumbs_up, thumbs_down, info, info_i, close, tune, done, dislike_btn, like_btn, logo, clock_icon, noimage, backGreen}
+export {clock, closeBtn, thumbs_up, thumbs_down, info, info_i, close, tune, done, dislike_btn, like_btn, logo, clock_icon, noimage, backGreen, backBlue}
 
 
