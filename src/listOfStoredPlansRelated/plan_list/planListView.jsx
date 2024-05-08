b53aa2b42a34538fdfd8b00f4c1089267a85174f
Gustav Eriksson <gustavke@kth.se>
2024-05-08 16:07:10 +0200

    added styling to planlist

--- src/listOfStoredPlansRelated/plan_list/planListView.jsx
+++ src/listOfStoredPlansRelated/plan_list/planListView.jsx
@@ -19,8 +19,8 @@ const PlanListView = (props) => {
   const dispatch = useDispatch();
 
   const renderMeals = (recipesName) => {
-    return recipesName.map((recipe) => {
-      return (<p className="text-[18px] font-outfit text-whiteSmoke truncate mt-2 mb-2">{recipe.name}</p>);
+    return recipesName.map((recipe, index) => {
+      return (<p key={index} className="text-[14px] font-outfit text-whiteSmoke truncate mb-1">{recipe.name}</p>);
     });
   };
   const RecepiePlanOverviewRow = (list) => {
@@ -38,20 +38,20 @@ const PlanListView = (props) => {
 
     return (
       <div className="flex flex-row">
-        <button className="bg-cerulean rounded-2xl my-4 px-4 shadow-xl w-[840px] h-[180px] content-center hover:brightness-75 hover:shadow-mid"
+        <div className="bg-cerulean  items-start rounded-2xl mt-4 shadow-xl w-full h-fit hover:brightness-110 hover:shadow-mid"
           onClick={() => { props.selectAndNavigateToMealPlan(list.planID) }}>
-          <div className="grid grid-cols-4 gap-4">
-            <div className="border-r border-whiteSmoke flex justify-center">
-              <div>
-                <p className="text-[24px] font-outfit font-medium text-whiteSmoke">meal</p>
-                <p className="text-[64px] font-outfit font-bold text-whiteSmoke flex justify-center">{list.planID}</p>
+          <div className="flex flex-col justify-center ">
+            <div className="border-b  border-whiteSmoke flex self-center  pt-4 pb-3 justify-center w-full">
+              <div className="flex flex-row w-full">
+                <p className="text-[16px] w-[92%] font-outfit  pl-[4%] font-bold text-whiteSmoke flex justify-center">{list.planName}</p>
+                <button className="w-[4%] aspect-square bg-cerulean  flex items-center justify-center"
+                  onClick={(event) => handleDelete(event, list.planID)} aria-label="Delete meal plan">
+                  <img src={closeBtn} />
+                </button>
               </div>
             </div>
-            <div>
-              {renderMeals(list.planRecipes.slice(0, 4))}
-            </div>
-            <div>
-              {renderMeals(list.planRecipes.slice(4, 7))}
+            <div className="pt-5 pb-5 pl-6 pr-6">
+              {renderMeals(list.planRecipes)}
             </div>
             <div className="relative">
               {/*add remove button here*/}
@@ -62,12 +62,9 @@ const PlanListView = (props) => {
           </div>
 
           {/* Possible iteration of the recipe names contained wtihin a plan. Make so all contents are displayed and possibly overflow to a new row. Until that this is implemented, make the line above's content conspicously large */}
-        </button>
+        </div>
 
-        <button className="ml-4 flex justify-center items-center rounded-md hover:brightness-200"
-          onClick={(event) => handleDelete(event, list.planID)} aria-label="Delete meal plan">
-          <img src={closeBtn} />
-        </button>
+        
       </div>
 
 
@@ -97,21 +94,19 @@ const PlanListView = (props) => {
   }
   else if (props.serverState === "ready") {
     return (
-      <div className="pt-32 bg-smokeWhite min-h-screen w-full top-0 right-0 bottom-0 left-0 flex justify-center">
-        <div className="w-[840px] mr-10 ml-10">
-          <div>
-            <button className="text-whiteSmoke hover:shadow-xl"
-              onClick={() => props.navigateBack()} aria-label="go back">
-              <img src={backBlack} />
-            </button>
-          </div>
-          <div>
-            <p className="text-[48px] font-outfit font-bold text-gunmetal tracking-wider">MEAL PLANS</p>
-          </div>
+      <div className="pt-32 flex flex-col bg-smokeWhite min-h-screen w-screen pr-2 pl-2 lg:pr-80 ">
+        <div className="self-center max-w-[840px] w-full">
+
+          <button className="text-whiteSmoke hover:shadow-xl "
+            onClick={() => props.navigateBack()} aria-label="go back">
+            <img src={backBlack} />
+          </button>
+          <p className="text-[48px] w-full text-center font-outfit font-bold text-gunmetal tracking-wider">MEAL PLANS</p>
           <div>
             {renderList()}
           </div>
         </div>
+
       </div>
 
 
