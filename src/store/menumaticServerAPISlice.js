4088562f749ecfa2ed1798521b26c25fee0cd6ad
Daniel3178 <Daniel.ibrahimi360@gmail.com>
2024-05-14 11:16:53 +0200

    Revert "Merge branch 'daniel-BUGFIX-LatestMealPlan' into feature"
    This reverts commit ad49b1a8fef7ceda718832fdf1f7f646bf24ae38, reversing
    changes made to b66bc04d6da25641e18af3d9119253778eb3a948.

--- src/store/menumaticServerAPISlice.js
+++ src/store/menumaticServerAPISlice.js
@@ -48,12 +48,6 @@ const menumaticServerApi = createSlice({
         (item) => item.id !== listId
       );
       state.userAllListPromise.data = updatedData;
-      if(updatedData.length > 0){
-        state.latestMealPlanPromise.data = updatedData[updatedData.length - 1];
-      }
-      else{
-        state.latestMealPlanPromise.data = {};
-      }
     },
     setSelectedList: (state, action) => {
       const { id } = action.payload;
