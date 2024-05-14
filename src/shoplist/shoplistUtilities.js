fd5a80f66e205fd8d6b0e36ae6995027f2f630d6
Daniel Ibrahimi <89932502+Daniel3178@users.noreply.github.com>
2024-05-15 00:25:42 +0200

    Daniel fix enhanced removed items (#22)
    * Release 3.5 (#20)
    
    * SPRINT 4 - FEATURES (#19)
    
    * drop in ocean fix recipe pdf
    
    * recipe PDF button removed
    
    * Export pdf for recipes is functional
    
    * Buttons are placed on top of each other
    
    ---------
    
    Co-authored-by: jonathansson <jonathansson98@gmail.com>
    
    * Revert "SPRINT 4 - FEATURES (#19)"
    
    This reverts commit 59508946a8bc6cb6b36cc50a9b455862f8711df7.
    
    * Reapply "SPRINT 4 - FEATURES (#19)"
    
    This reverts commit e57c719e1c811dcaa245307d2a797e8420afd686.
    
    * Reduced the number of requests sent to Spoonacular
    
    ---------
    
    Co-authored-by: jonathansson <jonathansson98@gmail.com>
    
    * release 3.5
    
    * The code for removing and restoring items are optimized, a bug in shop list view is fixed
    
    ---------
    
    Co-authored-by: jonathansson <jonathansson98@gmail.com>

--- src/shoplist/shoplistUtilities.js
+++ src/shoplist/shoplistUtilities.js
@@ -118,18 +118,34 @@ export const generateShopListUtil = (props) => {
 export const removeItemUtil = (prop) => {
   const { payload, allItems, removedItems } = prop;
 
-  const resultAllItem = [];
-  allItems.forEach((category) => {
-    resultAllItem.push({
-      category: category.category,
-      ingredients: parseIngredients(category.ingredients),
-    });
-  });
-  resultAllItem.map((category) => {
+  const resultAllItem = allItems;
+  // const tempItems = allItems;
+  // console.log("ALL ITEMS STATE TYPE", tempItems);
+  // allItems.forEach((category) => {
+  //   resultAllItem.push({
+  //     category: category.category,
+  //     ingredients: parseIngredients(category.ingredients),
+  //   });
+  // });
+  //  resultAllItem.forEach((category) => {
+  //   category.ingredients = category.ingredients.filter(
+  //     (item) => item.name !== payload.name
+  //   );
+  // });
+
+  const resultAllItemNonEmpty = resultAllItem.filter((category) => {
     category.ingredients = category.ingredients.filter(
       (item) => item.name !== payload.name
     );
+    return category.ingredients.length > 0;
   });
+
+  // console.log("BEFORE FILTER", resultAllItem);
+  // const resultAllItemNonEmpty = resultAllItem.filter(
+  //   (categ) => categ.ingredients.length > 0
+  // );
+  // console.log("AFTER FILTER", resultAllItemNonEmpty);
+
   const resultRemovedItem = [];
   removedItems.forEach((category) => {
     resultRemovedItem.push({
@@ -148,30 +164,42 @@ export const removeItemUtil = (prop) => {
   } else {
     resultRemovedItem[index].ingredients.push(payload);
   }
-  return { allItems: resultAllItem, removedItems: resultRemovedItem };
+  return { allItems: resultAllItemNonEmpty, removedItems: resultRemovedItem };
 };
 
 export const restoreItemUtil = (prop) => {
   const { payload, allItems, removedItems } = prop;
-  const resultRemovedItems = [];
-  removedItems.forEach((category) => {
-    resultRemovedItems.push({
-      category: category.category,
-      ingredients: parseIngredients(category.ingredients),
-    });
-  });
-  resultRemovedItems.map((category) => {
+  // const resultRemovedItems = [];
+  const resultRemovedItems = removedItems;
+
+  // removedItems.forEach((category) => {
+  //   resultRemovedItems.push({
+  //     category: category.category,
+  //     ingredients: parseIngredients(category.ingredients),
+  //   });
+  // });
+
+  // const resultRemovedItemsNonEmpty =  resultRemovedItems.forEach((category) => {
+  //   category.ingredients = category.ingredients.filter(
+  //     (item) => item.name !== payload.name
+  //   );
+  // });
+
+  const resultRemovedItemsNonEmpty = resultRemovedItems.filter((category) => {
     category.ingredients = category.ingredients.filter(
       (item) => item.name !== payload.name
     );
+    return category.ingredients.length > 0;
   });
-  const resultAllItems = [];
-  allItems.forEach((category) => {
-    resultAllItems.push({
-      category: category.category,
-      ingredients: parseIngredients(category.ingredients),
-    });
-  });
+
+  // const resultAllItems = [];
+  const resultAllItems = allItems;
+  // allItems.forEach((category) => {
+  //   resultAllItems.push({
+  //     category: category.category,
+  //     ingredients: parseIngredients(category.ingredients),
+  //   });
+  // });
 
   const index = resultAllItems.findIndex(
     (item) => item.category === payload.category
@@ -185,5 +213,5 @@ export const restoreItemUtil = (prop) => {
     resultAllItems[index].ingredients.push(payload);
   }
 
-  return { allItems: resultAllItems, removedItems: resultRemovedItems };
+  return { allItems: resultAllItems, removedItems: resultRemovedItemsNonEmpty };
 };
